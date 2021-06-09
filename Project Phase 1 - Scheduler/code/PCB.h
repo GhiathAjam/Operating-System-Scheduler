9370da87a89769244f8d2db618a1daea2faad096
Makrion <miklaziz3112000@gmail.com>
2021-06-09 05:51:30 +0200

    unpdatedpcd

--- Project Phase 1 - Scheduler/code/PCB.h
+++ Project Phase 1 - Scheduler/code/PCB.h
@@ -1,13 +1,16 @@
 #pragma once
+#include <sys/file.h>
+#include <string.h>
+#include<unistd.h>
 #include <malloc.h>
 #include "process_model.h"
 
 enum Process_State
 {
-STARTED,
-STOPED,
-RESUMED,
-FINISHED
+    STARTED,
+    STOPED,
+    RESUMED,
+    FINISHED
 } Process_State;
 
 struct PCB
@@ -19,34 +22,76 @@ struct PCB
     int priority;
     int remaining_time; //shared variable
     int waiting_time;
+    int finish_time;
     enum Process_State state;
 };
 
-
-struct PCB * new_pcb(process * p,int pid)
+struct PCB *new_pcb(process *p, int pid)
 {
-    struct PCB *pcb=(struct PCB *)malloc(sizeof(struct PCB));
-    pcb->id=p->pid;
-    pcb->pid=pid;
-    pcb->arrival_time=p->arrival_time;
-    pcb->run_time=p->run_time;
-    pcb->priority= p->priority;
-    pcb->remaining_time=p->run_time;
-    pcb->state=-1;
-    pcb->waiting_time=0;
+    struct PCB *pcb = (struct PCB *)malloc(sizeof(struct PCB));
+    pcb->id = p->pid;
+    pcb->pid = pid;
+    pcb->arrival_time = p->arrival_time;
+    pcb->run_time = p->run_time;
+    pcb->priority = p->priority;
+    pcb->remaining_time = p->run_time;
+    pcb->state = -1;
+    pcb->waiting_time = 0;
+    pcb->finish_time = 0;
     return pcb;
 }
 
-int turn_around(struct PCB* pcb,int finished_time)
+int turn_around(struct PCB *pcb)
 {
-    return (finished_time-(pcb->arrival_time));
+    return ((pcb->finish_time) - (pcb->arrival_time));
 }
-int wta(struct PCB* pcb,int finished_time)
+double wta(struct PCB *pcb)
 {
-    return (finished_time-(pcb->arrival_time))/(pcb->run_time);
+    double value = (double)((pcb->finish_time) - (pcb->arrival_time)) / (pcb->run_time);
+
+    int v = (int)(value * 100 + .5);
+    double rounded = v / 100.0;
+
+    return rounded;
 }
 //just for the testing
-void printpcb(struct PCB* pcb)
+void printpcb(struct PCB *pcb, int curr_time)
 {
-    printf("id=%d, pid=%d, arrival_time=%d, run_time=%d, priority=%d, remaining_time=%d, waiting_time=%d, state=%d \n", pcb->id,pcb->pid,pcb->arrival_time,pcb->run_time,pcb->priority,pcb->remaining_time,pcb->waiting_time,pcb-> state);
+
+    FILE *filePointer;
+    if (!(access("scheduler.log", F_OK) == 0))
+    {
+        filePointer = fopen("scheduler.log", "a+");
+        fprintf(filePointer,"AT time x process y  state arr w total z remain y wait k\n");
+    }
+    else
+        filePointer = fopen("scheduler.log", "a+");
+
+    char state[10];
+    switch (pcb->state)
+    {
+    case STARTED:
+        strcpy(state, "started");
+        break;
+    case STOPED:
+        strcpy(state, "stoped");
+        break;
+    case RESUMED:
+        strcpy(state, "resumed");
+        break;
+    case FINISHED:
+        strcpy(state, "finished");
+        break;
+    }
+
+
+
+
+    fprintf(filePointer,"AT time %d process %d %s arr %d total %d remain %d wait %d", curr_time, pcb->id, state, pcb->arrival_time, pcb->run_time, pcb->remaining_time, pcb->waiting_time);
+    if ((pcb->state) == FINISHED)
+    {
+        fprintf(filePointer," TA %d  WTA %.2f", turn_around(pcb), wta(pcb));
+    }
+    fprintf(filePointer,"\n");
+    fclose(filePointer);
 }
